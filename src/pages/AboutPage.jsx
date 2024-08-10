9a6f36629f32714c7e3806f5c334e0fa40d84b03
Aman Kumar <136882937+Aman-9454@users.noreply.github.com>
2024-08-10 20:21:44 +0530

    updated

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,6 +7,7 @@ import image3 from "../assets/c6.jpg";
 import mission from "../assets/complete-the-mission.svg";
 import TopInstructorCard from "../components/card/TopInstructorCard";
 import Loader from "../components/loader/Loader";
+
 const AboutPage = () => {
   const [loading, setLoading] = useState(true);
   const cardsData = [
@@ -22,7 +23,7 @@ const AboutPage = () => {
     },
     {
       title: "Fred Louis",
-      description: "To build abusiness sustainability",
+      description: "To build a business sustainability",
       image: image3,
     },
   ];
@@ -32,6 +33,7 @@ const AboutPage = () => {
       setLoading(false);
     }, 2000);
   }, []);
+
   return (
     <>
       {loading ? (
@@ -43,69 +45,107 @@ const AboutPage = () => {
           <section className="bg-[#f5f5dc] mb-8">
             <div className="max-w-screen-xl mx-auto px-6 ">
               <div className="grid grid-cols-1 sm:grid-cols-2 ">
-                <div className=" py-12 pr-8">
-                  <h1 className="text-4xl mb-5 font-sans">The Mignite Story</h1>
-                  <p className="text-[#800020]   font-bold text-xl ">
-                    Started in the year 2024, Mignite is one of the largest
-                    edtech platforms in India providing education to more than
-                    10 million users.
+                <div className="py-12 pr-8">
+                  <h1 className="text-4xl mb-5 font-sans">
+                    About Pragati Coaching Classes
+                  </h1>
+                  <p className="text-[#000] font-normal text-xl ">
+                    Pragati Coaching Classes is dedicated to empowering students
+                    to achieve academic excellence and personal growth. With a
+                    team of experienced educators and a student-centric
+                    approach, we provide comprehensive coaching that covers
+                    every aspect of learning. Our goal is to help students build
+                    a strong foundation, excel in their studies, and develop the
+                    skills needed for lifelong success. At Pragati, we believe
+                    in nurturing potential and guiding each student on their
+                    path to success.
                   </p>
-                  <p className="text-[#800020] mb-10 font-bold text-xl ">
-                    To allow every child to realize his/her dream, live up to
-                    their true potential and be their lifelong learning partner.
-                  </p>
-
-                  <span className="  border border-[#5e1920] text-2xl  hover:bg-gray-400 hover:border-gray-600 p-4 rounded cursor-pointer">
-                    Register Now
-                  </span>
                 </div>
-                <div>
+                <div className="flex justify-center items-center">
                   <SingleBanner image={image} />
                 </div>
               </div>
             </div>
           </section>
+
           <section className="bg-[#f5f5dc] py-10 mb-8">
-            <div className="container mx-auto p-4 my-4">
-              <h1 className="text-4xl text-center font-medium text-[#800020] text-400  mb-4">
-                Our Mission
-              </h1>
-              <div className="max-w-screen-xl mx-auto px-6 ">
+            <div className="container pl-10">
+              <h1 className="text-4xl mb-5 font-sans">Our Mission</h1>
+              <div className="max-w-screen-xl mx-auto px-6">
                 <div className="grid grid-cols-1 sm:grid-cols-2 ">
-                  <div className=" py-12 pr-8">
-                    <p className="text-[#800020] font-bold text-xl ">
-                      Mignite is the result of a continual effort to
-                      exponentially increase the employability of every Indian,
-                      irrespective of their socioeconomic status. With
-                      accessibility and affordability being the support
-                      structure of high-quality, industry-relevant courses,
-                      Mignite aims to empower professionals and students alike
-                      to either jumpstart their careers or leverage existing
-                      skills with new, future-driven upgrades that will help
-                      them realise their full potential.
-                    </p>
+                  <div className="py-0 pr-8">
+                    <ul className="text-[#000] font-normal text-xl list-disc pl-5">
+                      <li>
+                        <span className="text-[#000] font-bold">
+                          {" "}
+                          Foster Academic Excellence:{" "}
+                        </span>{" "}
+                        Provide high-quality education that enables students to
+                        achieve their full academic potential.
+                      </li>
+                      <br />
+                      <li>
+                        {" "}
+                        <span className="text-[#000] font-bold">
+                          Inspire Lifelong Learning:
+                        </span>{" "}
+                        Cultivate a love for learning that encourages students
+                        to continually grow and evolve in their educational
+                        journey.
+                      </li>
+                      <br />
+                      <li>
+                        {" "}
+                        <span className="text-[#000] font-bold">
+                          Empower Students:{" "}
+                        </span>
+                        Equip students with the skills, confidence, and
+                        knowledge needed to succeed in their academic and
+                        professional lives.
+                      </li>
+                      <br />
+                      <li>
+                        <span className="text-[#000] font-bold">
+                          Promote Holistic Development:
+                        </span>{" "}
+                        Focus on the overall development of students by
+                        nurturing their intellectual, emotional, and social
+                        growth.
+                      </li>
+                      <br />
+                      <li>
+                        {" "}
+                        <span className="text-[#000] font-bold">
+                          {" "}
+                          Support and Guide:{" "}
+                        </span>
+                        Offer personalized guidance and support to help each
+                        student overcome challenges and reach their goals.
+                      </li>
+                    </ul>
                   </div>
-                  <div>
+                  <div className="flex justify-center items-center">
                     <SingleBanner image={mission} />
                   </div>
                 </div>
               </div>
             </div>
           </section>
-          <section className="bg-[#f5f5dc] py-10 mb-8">
-            <div className="container mx-auto p-4 my-4">
-              <h1 className="text-4xl font-medium text-[#800020] text-400  mb-8">
-                Our Vision
-              </h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {cardsData.map((card, index) => (
-                  <TopInstructorCard
-                    key={index}
-                    // title={card.title}
-                    description={card.description}
-                    image={card.image}
-                  />
-                ))}
+
+          <section className="bg-[#f5f5dc] mb-8">
+            <div className="max-w-screen-xl mx-auto px-6 ">
+              <div className="grid grid-cols-1 sm:grid-cols-2 ">
+                <div className="py-12 pr-8">
+                  <h1 className="text-4xl mb-5 font-sans">
+                  Our Vision
+                  </h1>
+                  <p className="text-[#000] font-normal text-xl ">
+                  Pragati Coaching Classes is to be a leading educational institution that transforms lives through quality education. We aspire to create a nurturing environment where every student is inspired to achieve academic excellence, develop a passion for lifelong learning, and become a confident, responsible, and successful individual. We aim to empower our students to not only excel in their studies but also to contribute positively to society, shaping a brighter future for themselves and the world.
+                  </p>
+                </div>
+                <div className="flex justify-center items-center">
+                  <SingleBanner image={image} />
+                </div>
               </div>
             </div>
           </section>
